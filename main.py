4409ef7b9873f8bbea3a7ca35cded6bb074cfc0d
r.w. <rebecca.wells226@gmail.com>
2026-05-04 12:19:53 -0500

    changed file name for better importing experience

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from my_class&methods.py
+#create custom error handling for input?
 
+#getting user input
+latitude = float(input("Enter latitude: "))
+longitude = float(input("Enter longitude: "))
+month = int(input("Enter month in numerical form: "))
+day = int(input("Enter day: "))
+year = int(input("Enter year: "))
 
-print("This is a test")
+#set class to variable
+weather = WeatherData(latitude,longitude,month,day,year)
+
+weather.get_data()
