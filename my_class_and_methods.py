88d3434a6518ace540a6d136102492576f3b18a1
r.w. <rebecca.wells226@gmail.com>
2026-05-04 16:56:36 -0500

    made api call

--- my_class_and_methods.py
+++ my_class_and_methods.py
@@ -40,9 +40,29 @@ class WeatherData:
 
         #api call here- will use loop to append values to lists
 
-            for i in range(self.year-5,self.year):
+        for yr in range(self.year-5, self.year):
+            date = f"{yr:04d}-{self.month:02d}-{self.day:02d}"
+            url = "https://archive-api.open-meteo.com/v1/archive"
 
-        #aggregation time!!!
+            query_params = {
+                "latitude": self.latitude,
+                'longitude': self.longitude,
+                "start_date": date,
+                "end_date": date,
+                "daily": "temperature_2m_mean,precipitation_sum,wind_speed_10m_max",
+                "temperature_unit": "fahrenheit",
+                "wind_speed_unit": "mph",
+                "precipitation_unit": "inch"
+            }
+
+            response = requests.get(url,params=query_params)
+
+            #response.json()
+
+
+
+
+        ''''#aggregation time!!!
         # temperature
         self.temp_avg5yr = sum(temps) / len(temps)
         self.temp_min5yr = min(temps)
@@ -56,7 +76,7 @@ class WeatherData:
         # precipitation
         self.precip_sum5yr = sum(precip)
         self.precip_min5yr = min(precip)
-        self.precip_max5yr = max(precip)
+        self.precip_max5yr = max(precip'''
 
 
 
