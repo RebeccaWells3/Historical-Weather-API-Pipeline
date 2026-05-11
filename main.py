d8143d63ddc1abfaf29965e8a214f662ef4184eb
r.w. <rebecca.wells226@gmail.com>
2026-05-11 14:56:12 -0500

    left self note about validation for inputs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from my_class_and_methods import WeatherData
 
+#add validation for input values
+
 #getting user input
 latitude = float(input("Enter latitude: "))
 longitude = float(input("Enter longitude: "))
