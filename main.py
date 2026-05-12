aed78a5558192330868e9ce7c7dc23182789913e
r.w. <rebecca.wells226@gmail.com>
2026-05-12 14:09:17 -0500

    c4 table populated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
 from my_class_and_methods import WeatherData
-from table class in SQLite import
+from class_sqlalchemy import WeatherRecord, session
+
 #add validation for input values
 
 #getting user input
@@ -10,10 +10,32 @@ month = int(input("Enter month in numerical form: "))
 day = int(input("Enter day: "))
 year = int(input("Enter year: "))
 
-#c3
+
 #create instance of class
 weather = WeatherData(latitude,longitude,month,day,year)
 
 #call class method
 weather.get_data()
+
+#
+record = WeatherRecord(
+    latitude = weather.latitude,
+    longitude = weather.longitude,
+    month = weather.month,
+    day = weather.day,
+    year = weather.year,
+    temp_avg5yr = weather.temp_avg5yr,
+    temp_min5yr = weather.temp_min5yr,
+    temp_max5yr = weather.temp_max5yr,
+    wind_avg5yr = weather.wind_avg5yr,
+    wind_min5yr = weather.wind_min5yr,
+    wind_max5yr = weather.wind_max5yr,
+    precip_sum5yr = weather.precip_sum5yr,
+    precip_min5yr = weather.precip_min5yr,
+    precip_max5yr = weather.precip_max5yr
+)
+
+#populates table
+session.add(record)
+session.commit()
 
